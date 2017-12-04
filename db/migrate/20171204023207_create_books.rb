c86b1a85ec705c4acf04cbb294117fe4570cf9da
TaigaMikami <mikamimikami60@gmail.com>
2017-12-05 05:09:39 +0900

    show page index page

--- db/migrate/20171204023207_create_books.rb
+++ db/migrate/20171204023207_create_books.rb
@@ -5,7 +5,7 @@ class CreateBooks < ActiveRecord::Migration[5.1]
       t.string :author
       t.text :description
       t.integer :isbn, :limit => 8
-      t.date :publishdate
+      t.string :publishdate
 
       t.timestamps
     end
