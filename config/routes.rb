069fcd680ad8ef1c418f503a5f5d0e2ce839485e
TaigaMikami <mikamimikami60@gmail.com>
2017-12-04 11:26:37 +0900

    バグ fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
+  get '/books/new', to: 'books#new'
 
-  get 'books/new', to: 'books#new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
